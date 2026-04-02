3e3c19a8d69b68c5ffd78836a23a7221ed669493
Marzooqe Moideen <marzooqe@gmail.com>
2026-04-02 11:33:21 +0200

    Initial commit: dbt project with Docker

--- hf_pg_dbt/analyses/query_1_financial_&_variance_analysis.sql
+++ hf_pg_dbt/analyses/query_1_financial_&_variance_analysis.sql
@@ -15,18 +15,12 @@ with_cost AS (
     FROM with_area a
     INNER join transform.dim_procurement_cost  pc ON  a.market = pc.market
         AND a.order_date BETWEEN pc.valid_from AND pc.valid_to
-),
-with_eur AS (
-    SELECT
-        *,
-        surface_area_m2_normalised * cost_per_m2_eur AS order_cost_eur
-    FROM with_cost
 )
 SELECT
     market,
     COUNT(order_id) AS order_count,
     (SUM(surface_area_m2_normalised)) AS total_surface_m2,
-    (SUM(order_cost_eur)) AS total_cost_eur
-FROM with_eur
+    (SUM(surface_area_m2_normalised * cost_per_m2_eur)) AS total_cost_eur
+FROM with_cost
 GROUP BY market
 ORDER BY total_cost_eur DESC
